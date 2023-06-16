42d75f89725146aefc79923d34a82eecd0b12e22
codigomaestro <williamsroland147@outlook.com>
2023-06-17 00:05:42 +0100

    program that prints the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,9 +11,14 @@ int main(void)
 	int n, last_digit;
 
 	srand(time(0));
-	n= rand() - RAND_MAX / 2;
-	
-	last_digit = n % 10;
+	n = rand() - RAND_MAX / 2;
+
+	last_digit = abs(n % 10);
+
+	if (n < 0)
+	{
+		last_digit = -last_digit;
+	}
 	
 	if (last_digit > 5)
 	{
