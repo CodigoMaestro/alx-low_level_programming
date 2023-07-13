aad6b7f087c572a0a853ae918f6cb065a08cafe0
codigomaestro <williamsroland147@outlook.com>
2023-07-13 20:26:29 +0100

    function that concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,28 +25,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-
-	while (s1[s1_len] != '\0')
+	if (s1[s1_len] != '\0')
 	{
 		s1_len++;
 	}
-	while (s2[s2_len] != '\0')
+	if (s2[s2_len] != '\0')
 	{
 		s2_len++;
 	}
-
 	str = malloc((s1_len + n + 1) * sizeof(char));
-
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < s1_len; i++)
 	{
 		str[i] = s1[i];
 	}
-
 	for (j = 0; j < n; j++)
 	{
 		str[i + j] = s2[j];
