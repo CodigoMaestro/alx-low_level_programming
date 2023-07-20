07075e86222e1f657b8e36c862d1855e56e54f35
codigomaestro <williamsroland147@outlook.com>
2023-07-20 04:53:23 +0100

    program that performs simple operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(op)(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
