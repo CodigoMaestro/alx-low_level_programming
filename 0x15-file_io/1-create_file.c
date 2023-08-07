b3d8e3203723765254a070259a1808c159f42863
codigomaestro <williamsroland147@outlook.com>
2023-08-07 21:43:17 +0100

    create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,20 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int len;
 	ssize_t textWrite;
 
 	if (filename == NULL)
 	{
 		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		{
+			len++;
+		}
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
@@ -28,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	textWrite = write(fd, text_content, strlen(text_content));
+	textWrite = write(fd, text_content, len);
 
 	if (textWrite == -1)
 	{
