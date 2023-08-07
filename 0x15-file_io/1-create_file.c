d6bb550fe6b96c009c59d1277ebc13dcb4df0041
codigomaestro <williamsroland147@outlook.com>
2023-08-07 21:54:52 +0100

    create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 	{
