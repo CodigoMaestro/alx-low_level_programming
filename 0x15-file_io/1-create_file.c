6b0c28e450aeba36ff43236216a4012f6d76e670
codigomaestro <williamsroland147@outlook.com>
2023-08-07 21:17:30 +0100

    create a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - entry point
+ * @filename: char
+ * @text_content: char
+ * Return: int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t textWrite;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	textWrite = write(fd, text_content, strlen(text_content));
+
+	if (textWrite == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
